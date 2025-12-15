c946010b7fa753d1d610652b275fc447488d6d0e
Wincenty Szymura <szymura.wincenty@gmail.com>
2025-12-15 18:19:23 +0100

    adjusted model loading to improve security

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 # import relevant libraries
 from fastai.vision.all import *
+#from pathlib import Path
+from safetensors.torch import load_file
 import gradio as gr
+import timm
 
-# load the model
-learn = load_learner('AstroVisionModel.pkl')
+# build minimal dataloaders with transformations reflecting training
+dls = ImageDataLoaders.from_folder(
+    'example_data',
+    valid_pct=0.5,
+    item_tfms=Resize(224, method='squish'),
+    bs=1
+)
+
+# recreate the training architecture
+learn = vision_learner(dls, 'convnext_tiny.fb_in22k')
+
+# load trained model weights
+state = load_file('astro_vision_model.safetensors')
+learn.model.load_state_dict(state, strict=True)
+learn.model.eval()
 
 # define output categories
-categories = ("Gravitational lensing", "No lensing")
+categories = ['Gravitational Lensing', 'No Lensing']
 
 # define function that gradio is going to call
 def classify_image(image):
@@ -18,7 +34,11 @@ def classify_image(image):
 # define input, output and examples
 image = gr.Image(height=224, width=224)
 label = gr.Label()
-examples = ['test_n.jpg', 'test_u2.jpg', 'test_y.jpg']
+examples = [
+    'example_data/gravitational_lensing/test_y.jpg',
+    'example_data/no_lensing/test_n.jpg',
+    'example_data/no_lensing/test_u2.jpg',
+]
 
 # define and launch the interface
 interface = gr.Interface(
